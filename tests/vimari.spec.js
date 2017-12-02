f3bd5885bad5d5477fe47e8cc347248b30ff42e5
Simon Egersand <s.egersand@gmail.com>
2017-12-02 23:59:58 +0100

    Actually enable testing possibilites
    The changes made when the previous test was introduced broke the
    implementation, this solution respects the implementation as well as
    enables testing possibilites.

--- tests/vimari.spec.js
+++ tests/vimari.spec.js
@@ -1,18 +1,17 @@
 const expect = require('expect.js');
-const utils = require('../vimari.safariextension/utils');
 
 describe('utilites', () => {
     describe('isEnabledForUrl', () => {
         it('returns true on same exact domain', () => {
             const excludedUrl = 'http://specific-domain.com';
             const currentUrl = excludedUrl;
-            expect(utils.isEnabledForUrl(excludedUrl, currentUrl)).to.not.be.ok();
+            expect(window.isEnabledForUrl(excludedUrl, currentUrl)).to.not.be.ok();
         });
 
         it('returns false on slightly different domains', () => {
             const excludedUrls = 'http://www.specific-domain.com,www.specific-domain.com,specific-domain.com';
             const currentUrl = 'http://specific-domain.com';
-            expect(utils.isEnabledForUrl(excludedUrls, currentUrl)).to.be.ok();
+            expect(window.isEnabledForUrl(excludedUrls, currentUrl)).to.be.ok();
         });
     });
 });
