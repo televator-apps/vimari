c20ce3e2ad264aba0d39e7168d258c501b26115e
danielcompton <desk@danielcompton.net>
2019-09-15 18:14:21 -0500

    Add new generated Vimari project with net.televator bundle

---
+++ Vimari/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Vimari
+//
+//  Created by Daniel Compton on 15/09/19.
+//  Copyright © 2019 Televator. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
