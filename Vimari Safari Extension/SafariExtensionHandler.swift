7130efe83713dd482c6b1bfe96f1c90ee289a8cc
Simon Egersand <s.egersand@gmail.com>
2019-09-15 22:46:35 -0500

    Enable opening new empty tab
    Right now the page `localhost` will be opened. Ideally the page that
    opens would just be an empty page but I don't think Safari has such a
    page.

--- Vimari Safari Extension/SafariExtensionHandler.swift
+++ Vimari Safari Extension/SafariExtensionHandler.swift
@@ -4,6 +4,7 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     
     enum MessageType: String {
         case openLinkInTab
+        case openNewTab
     }
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
@@ -11,6 +12,9 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         case MessageType.openLinkInTab.rawValue:
             let url = URL(string: userInfo?["url"] as! String)
             openInNewTab(url: url!)
+            break
+        case MessageType.openNewTab.rawValue:
+            openNewTab()
             break
         default:
             NSLog("Received message with unsupported type: \(messageName)")
@@ -20,6 +24,16 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     func openInNewTab(url: URL) {
         SFSafariApplication.getActiveWindow { (activeWindow) in
             activeWindow?.openTab(with: url, makeActiveIfPossible: false, completionHandler: {_ in
+                // Perform some action here after the page loads
+            })
+        }
+    }
+
+    func openNewTab() {
+        // Ideally this URL would be something that represents an empty tab better than localhost
+        let url = URL(string: "http://localhost")!
+        SFSafariApplication.getActiveWindow { (activeWindow) in
+            activeWindow?.openTab(with: url, makeActiveIfPossible: true, completionHandler: {_ in
                 // Perform some action here after the page loads
             })
         }
