908bbafdaf22484a57fa02253a589b7f1f24fea4
Simon Egersand <s.egersand@gmail.com>
2019-09-15 23:17:13 -0500

    Add toolbar icon
    Until we know a proper way to configure settings we'll have to use the
    toolbar menu.

--- Vimari Safari Extension/SafariExtensionHandler.swift
+++ Vimari Safari Extension/SafariExtensionHandler.swift
@@ -38,4 +38,14 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
             })
         }
     }
+    
+    override func toolbarItemClicked(in window: SFSafariWindow) {
+        // This method will be called when your toolbar item is clicked.
+        NSLog("The extension's toolbar item was clicked")
+    }
+    
+    override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
+        // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
+        validationHandler(true, "")
+    }
 }
