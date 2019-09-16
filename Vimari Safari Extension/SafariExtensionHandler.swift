847d59d44de7830d9b488a408ad3019684f3040e
Simon Egersand <s.egersand@gmail.com>
2019-09-15 22:21:48 -0500

    Enable opening link in new tab
    However, it doesn't work to open multiple new links in tabs without
    reloading the page in the browser yet. It's a client side problem but
    I'm not sure why it happens yet though.

--- Vimari Safari Extension/SafariExtensionHandler.swift
+++ Vimari Safari Extension/SafariExtensionHandler.swift
@@ -2,25 +2,26 @@ import SafariServices
 
 class SafariExtensionHandler: SFSafariExtensionHandler {
     
+    enum MessageType: String {
+        case openLinkInTab
+    }
+    
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
-        // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
-        page.getPropertiesWithCompletionHandler { properties in
-            NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
+        switch messageName {
+        case MessageType.openLinkInTab.rawValue:
+            let url = URL(string: userInfo?["url"] as! String)
+            openInNewTab(url: url!)
+            break
+        default:
+            NSLog("Received message with unsupported type: \(messageName)")
         }
     }
     
-    override func toolbarItemClicked(in window: SFSafariWindow) {
-        // This method will be called when your toolbar item is clicked.
-        NSLog("The extension's toolbar item was clicked")
+    func openInNewTab(url: URL) {
+        SFSafariApplication.getActiveWindow { (activeWindow) in
+            activeWindow?.openTab(with: url, makeActiveIfPossible: false, completionHandler: {_ in
+                // Perform some action here after the page loads
+            })
+        }
     }
-    
-    override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
-        // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
-        validationHandler(true, "")
-    }
-    
-    override func popoverViewController() -> SFSafariExtensionViewController {
-        return SafariExtensionViewController.shared
-    }
-
 }
