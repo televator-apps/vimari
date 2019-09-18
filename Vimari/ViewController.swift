c9dfd59cd0d5305ef7d9e0f73ec11b7648ef7aff
Daniel Compton <desk@danielcompton.net>
2019-09-18 17:44:12 +1200

    Show state of Safari extension in main app

--- Vimari/ViewController.swift
+++ Vimari/ViewController.swift
@@ -4,10 +4,46 @@ import SafariServices.SFSafariApplication
 class ViewController: NSViewController {
 
     @IBOutlet var appNameLabel: NSTextField!
+    @IBOutlet var extensionStatus: NSTextField!
+    @IBOutlet var spinner: NSProgressIndicator!
+    
+    func refreshExtensionStatus () {
+        self.spinner.startAnimation(self)
+        self.extensionStatus.stringValue = "Checking extension status"
+        
+        if SFSafariServicesAvailable() {
+            SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: "net.televator.Vimari-Extension") {
+                (state, error) in
+                print(state as Any, error as Any, state?.isEnabled as Any)
+                
+                DispatchQueue.main.async {
+                    // TODO: handle this getting updated in the Safari preferences too.
+                    if let state = state {
+                        if state.isEnabled {
+                            self.extensionStatus.stringValue = "enabled"
+                        } else {
+                            self.extensionStatus.stringValue = "disabled"
+                        }
+                    }
+                    self.spinner.stopAnimation(self)
+                }
+            }
+        } else {
+            self.extensionStatus.stringValue = "Unavailable, Vimari requires Safari 10 or greater."
+            self.spinner.stopAnimation(self)
+        }
+    }
+    
+    @IBAction func refreshButton(_ sender: NSButton) {
+        refreshExtensionStatus()
+    }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         self.appNameLabel.stringValue = "Vimari";
+        
+        
+        refreshExtensionStatus();
     }
     
     @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
