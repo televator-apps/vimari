8770033bead3306e62b9c87c39b7ecf669612338
Nick Belzer <me@nickbelzer.me>
2020-08-27 17:42:26 +0200

    Replace default target

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-      .target(name: "Vimari", dependencies: [], path: "Vimari", sources: []),
+      .target(name: "Vimari", dependencies: ["SwiftLint"], path: "Vimari", sources: ["AppDelegate.Swift"]),
     ]
 )
