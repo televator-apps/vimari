08ca6f029124f25aaad2926b1abfd0e7a1d73d9d
Nick Belzer <me@nickbelzer.me>
2020-08-27 17:50:21 +0200

    Include product for target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "vimari",
+    products: [
+        .library(name: "Vimari", targets: ["Vimari"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -14,6 +17,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-      .target(name: "Vimari", dependencies: ["SwiftLint"], path: "Vimari", sources: ["AppDelegate.Swift"]),
+        .target(name: "Vimari", dependencies: [], path: "Vimari"),
     ]
 )
